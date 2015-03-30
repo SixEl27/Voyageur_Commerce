c0711695c2351a13f50850208232b3c5b623dc3f
SixEl27 <alexis.robitaille@orange.fr>
2015-03-30 16:40:57 +0200

    Modification de N-opt --> Plus d'erreur Youpi youpi yeah!

--- src/Nopt.java
+++ src/Nopt.java
@@ -10,10 +10,7 @@ public class Nopt {
 		// TODO Auto-generated method stub
 
 	}
-	
-	
-	
-	
+
 
 	public Graph Nopt(Graph G){
 		Graph H=new Graph(G.nomG);
@@ -27,18 +24,19 @@ public class Nopt {
 						Noeud xim1= H.RecupererNoeud(xi.idn-1);
 						Noeud xjp1= H.RecupererNoeud(xj.idn+1);
 						Noeud xjm1= H.RecupererNoeud(xj.idn-1);
-						
+
 						Arc xixip1 = new Arc(xi,xip1);
 						Arc xjxjp1 = new Arc(xj,xjp1);
 						Arc xixj = new Arc(xi,xj);
 						Arc xip1xjp1 = new Arc(xip1,xjp1);
-						
+
 						Route r_xixip1=(Route)xixip1;
 						Route r_xjxjp1=(Route)xjxjp1;
 						Route r_xixj=(Route)xixj;
 						Route r_xip1xjp1=(Route)xip1xjp1;
-						
+
 						if((r_xixip1.distance+r_xjxjp1.distance)>(r_xixj.distance+r_xip1xjp1.distance)){
+							//Verifier que
 							Route sauv=r_xixip1;
 							Route sauv2=r_xjxjp1;
 							r_xixip1=r_xixj;
@@ -50,27 +48,26 @@ public class Nopt {
 					}
 				}
 			}
+		}
 		return H;
 	}
-	
-		
-		
+}
+
 	//fonction 2-opt ( G : Graphe, H : CycleHamiltonien )
 
-	  //  amélioration : booléen := vrai
-	   // Tant que amélioration = vrai faire
+	//  amélioration : booléen := vrai
+	// Tant que amélioration = vrai faire
 
-	     //   amélioration := faux;
-	       // Pour tout sommet xi de H faire
+	//   amélioration := faux;
+	// Pour tout sommet xi de H faire
 
-	         //   Pour tout sommet xj de H, avec j différent de i-1 et i+1 faire
+	//   Pour tout sommet xj de H, avec j différent de i-1 et i+1 faire
 
-	           //     Si distance(xi, xi+1) + distance(xj, xj+1) > distance(xi, xj) + distance(xi+1, xj+1) alors
+	//     Si distance(xi, xi+1) + distance(xj, xj+1) > distance(xi, xj) + distance(xi+1, xj+1) alors
 
-	             //       Remplacer les arêtes (xi, xi+1) et (xj, xj+1) par (xi, xj) et (xi+1, xj+1) dans H
-	               //     amélioration := vrai;
+	//       Remplacer les arêtes (xi, xi+1) et (xj, xj+1) par (xi, xj) et (xi+1, xj+1) dans H
+	//     amélioration := vrai;
 
-	    //retourner H
+	//retourner H
 
 
-}
