1451b03421b5baded1bbb2b11495deaf1a1681b0
Alexis <Alexis@Alexis-PC>
2015-05-04 16:02:40 +0200

    Main_modif

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ceci est le main");
-		VDC vdc_test=new VDC("ceci est un probleme");
-		//System.out.println(vdc_test);
+	java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interface2().setVisible(true);
+            }
+        });
 	}
 
 }
