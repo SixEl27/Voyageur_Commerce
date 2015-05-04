7aed8edd6fd160ceaf03c1f8fd0b447d6851f14a
Alexis <Alexis@Alexis-PC>
2015-05-04 16:31:29 +0200

    Premiere essai Main (règle le problème des routes - distance)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-import javax.swing.JFrame;
 
+import javax.swing.JFrame;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	java.awt.EventQueue.invokeLater(new Runnable() {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        /**
+        java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Interface2().setVisible(true);
             }
         });
-	}
+        */
+        VDC VDC1 = new VDC("Essai");
+        Ville a = new Ville(0, 0, "a");
+        Ville b = new Ville(10, 10, "b");
+        VDC1.ajouterVille(a);
+        VDC1.ajouterVille(b);
+    }
 
 }
