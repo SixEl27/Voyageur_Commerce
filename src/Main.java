6c2fc8e49bc9b6d237f61036877186d4768720f3
Alexis <Alexis@Alexis-PC>
2015-05-15 16:17:21 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Interface().setVisible(true);
-            }
-        });
-        /**
-        VDC VDC1 = new VDC("Essai");
-        Ville a = new Ville(0, 0, "a");
-        Ville b = new Ville(10, 10, "b");
-        VDC1.ajouterVille(a);
-        VDC1.ajouterVille(b);
-        */
+        new Interface(new VDC("essai")).setVisible(true);
     }
 
 }
