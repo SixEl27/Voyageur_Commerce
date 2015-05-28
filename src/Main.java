af8aac8d142e460c9bfb3be487e28c035ec4ea40
acottin <aurelien.cottin@gmail.com>
2015-05-28 09:24:04 +0200

    main fait pour debugger IVPE

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.io.IOException;
 
-import javax.swing.JFrame;
 
 public class Main {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        new Interface(new VDC("essai")).setVisible(true);
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Interface(new VDC("essai")).setVisible(true);
+//		VDC vdc=new VDC("test");
+//		vdc.ajouterVille(new Ville(0,1,"a"));
+//		vdc.ajouterVille(new Ville(0,2,"b"));
+//		vdc.ajouterVille(new Ville(0,5,"c"));
+//		vdc.ajouterVille(new Ville(0,8,"d"));
+//		vdc.ajouterVille(new Ville(0,10,"e"));
+//		vdc.ajouterSolution(vdc.two_opt());
+//		vdc.ajouterSolution(vdc.insertionVoisinLePlusEloigne());
+//		System.out.println(vdc);
+//		for (Ville v : vdc.listeSolution.get(1)){
+//			System.out.println(v.nom);
+//		}
+	}
 
 }
